9882de4cad2adeb5aab5c12cafc723c15dbf1f7e
Alexander <fiireex@yandex.ru>
2023-03-21 18:51:15 +0300

    done tt2

--- TT2/Program.cs
+++ TT2/Program.cs
@@ -1,13 +1,12 @@
 ﻿Console.Clear();
 Console.WriteLine ("Введите трёхзначное число: ");
 string a = Console.ReadLine ()!;
-int sum = 0;
-    while(Convert.ToInt32(a) < 100 && Convert.ToInt32(a) > 999)
+    while(Convert.ToInt32(a) < 100 || Convert.ToInt32(a) > 999)
         {
         Console.WriteLine ("Введите трёхзначное число: ");
         a = Console.ReadLine ()!;
         }
-Console.WriteLine(Convert.ToInt32(a[0].ToString()) + Convert.ToInt32(a[1]. ToString));
+Console.WriteLine($"Сумма цифр в этом числе = {(Convert.ToInt32(a[0]. ToString()))+(Convert.ToInt32(a[1]. ToString()))+(Convert.ToInt32(a[2]. ToString()))}");
 
 
 
