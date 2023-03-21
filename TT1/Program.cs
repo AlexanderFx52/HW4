a14d12de9e1dbc0cf8fa4ca788fd5a26c37e2c51
Alexander <fiireex@yandex.ru>
2023-03-21 18:11:36 +0300

    Done tt1

---
+++ TT1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите Второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+int sum = a; 
+int q = a;
+    for(int i = 1; i < b; i++)
+{   q = q * sum;
+    
+}
+Console.WriteLine($"Ваше первое число {a} в степени {b} будет равно : {q}");
