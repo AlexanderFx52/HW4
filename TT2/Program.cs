ccd6f0efaad58eb1bd0fc004a05d84ebf2705e95
Alexander <fiireex@yandex.ru>
2023-03-21 18:38:08 +0300

    pause

---
+++ TT2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите трёхзначное число: ");
+string a = Console.ReadLine ()!;
+int sum = 0;
+    while(Convert.ToInt32(a) < 100 && Convert.ToInt32(a) > 999)
+        {
+        Console.WriteLine ("Введите трёхзначное число: ");
+        a = Console.ReadLine ()!;
+        }
+Console.WriteLine(Convert.ToInt32(a[0].ToString()) + Convert.ToInt32(a[1]. ToString));
+
+
+
