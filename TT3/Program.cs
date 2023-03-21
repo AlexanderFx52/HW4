840649f5612456fb36a9829675ad221605e9adf1
Alexander <fiireex@yandex.ru>
2023-03-21 19:15:15 +0300

    done tt3

---
+++ TT3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int n = 8;
+int[] a = new int[n];
+    for(int i = 0; i<a.Length; i++)
+        a[i] = new Random(). Next(1, 101);
+Console.WriteLine($"Вот твой массив, друг [{string.Join(", ", a)}]");
